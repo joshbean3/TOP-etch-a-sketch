189ebb5ea0c1cc3f60c8043a54544a1e89e624a3
joshbean3 <joshdbean3@gmail.com>
2025-07-10 20:51:54 -0400

    Add opacity increase with each hover

--- j-script.js
+++ j-script.js
@@ -18,21 +18,32 @@ btn.addEventListener("click", function() {
 function createGrid(squares) {
     for (let i=0; i< (squares ** 2); i++) {
         const newDiv = document.createElement("div");
+        // Set Div properites in the DOM for each Div
+        newDiv.hoverCount = 0;
+        newDiv.redColor = 0;
+        newDiv.blueColor = 0;
+        newDiv.greenColor = 0;
+        // Style each Div 
         newDiv.style.border = "1px solid red"
         newDiv.style.height = `${1600 / squares}px`;
         newDiv.style.width = `${1600 / squares}px`;
+        // List for hover
         newDiv.addEventListener("mouseover", function() {
-            // Get random numbers for RGB values
-            const random_red = getRandomArbitrary(0,255);
-            const random_blue = getRandomArbitrary(0,255);
-            const random_green = getRandomArbitrary(0,255);
-            // Apply RGB values to background color for squares
-            newDiv.style.backgroundColor = `rgb(${random_red},${random_blue},${random_green})`;
+            // Count each hover 
+            newDiv.hoverCount++;
+            // Get random numbers for RGB values only if it is the first hover
+            if (newDiv.hoverCount === 1) {
+                newDiv.redColor = getRandomArbitrary(0,255);
+                newDiv.blueColor = getRandomArbitrary(0,255);
+                newDiv.greenColor = getRandomArbitrary(0,255);
+            }
+            // Apply RGB values to background color for squares. Opacity is increased as a proportion of times hovered.
+            newDiv.style.backgroundColor = `rgba(${newDiv.redColor},${newDiv.blueColor},${newDiv.greenColor},${newDiv.hoverCount / 10})`;
         })
         gridContainer.appendChild(newDiv);
     }
 }
 // get random number between two values 
 function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
